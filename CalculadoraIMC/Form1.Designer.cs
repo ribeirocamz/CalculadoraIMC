ba5009ebe16950c7d3e216ba525ee3d1b614bdb1
Camila Ribeiro <camilaimoliveira67@gmail.com>
2025-08-18 22:23:24 -0300

    Implementação do código
    Foi criado código para a calculadora de IMC e implementação de tratamento de erros

--- CalculadoraIMC/Form1.Designer.cs
+++ CalculadoraIMC/Form1.Designer.cs
@@ -28,13 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.txbPeso = new System.Windows.Forms.TextBox();
+            this.txbAltura = new System.Windows.Forms.TextBox();
+            this.btnClacular = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblSaude = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(147, 55);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(255, 28);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Calculadora de IMC";
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Font = new System.Drawing.Font("Franklin Gothic Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPeso.Location = new System.Drawing.Point(87, 161);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(46, 21);
+            this.lblPeso.TabIndex = 1;
+            this.lblPeso.Text = "Peso:";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Font = new System.Drawing.Font("Franklin Gothic Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltura.Location = new System.Drawing.Point(79, 215);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(54, 21);
+            this.lblAltura.TabIndex = 2;
+            this.lblAltura.Text = "Altura:";
+            // 
+            // txbPeso
+            // 
+            this.txbPeso.Location = new System.Drawing.Point(139, 163);
+            this.txbPeso.Name = "txbPeso";
+            this.txbPeso.Size = new System.Drawing.Size(108, 20);
+            this.txbPeso.TabIndex = 3;
+            // 
+            // txbAltura
+            // 
+            this.txbAltura.Location = new System.Drawing.Point(139, 215);
+            this.txbAltura.Name = "txbAltura";
+            this.txbAltura.Size = new System.Drawing.Size(108, 20);
+            this.txbAltura.TabIndex = 4;
+            // 
+            // btnClacular
+            // 
+            this.btnClacular.Location = new System.Drawing.Point(71, 251);
+            this.btnClacular.Name = "btnClacular";
+            this.btnClacular.Size = new System.Drawing.Size(176, 35);
+            this.btnClacular.TabIndex = 5;
+            this.btnClacular.Text = "Calcular";
+            this.btnClacular.UseVisualStyleBackColor = true;
+            this.btnClacular.Click += new System.EventHandler(this.btnClacular_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Franklin Gothic Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(67, 307);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(78, 21);
+            this.lblResultado.TabIndex = 6;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // lblSaude
+            // 
+            this.lblSaude.AutoSize = true;
+            this.lblSaude.Location = new System.Drawing.Point(71, 354);
+            this.lblSaude.Name = "lblSaude";
+            this.lblSaude.Size = new System.Drawing.Size(0, 13);
+            this.lblSaude.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(588, 445);
+            this.Controls.Add(this.lblSaude);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnClacular);
+            this.Controls.Add(this.txbAltura);
+            this.Controls.Add(this.txbPeso);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora de IMC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.TextBox txbPeso;
+        private System.Windows.Forms.TextBox txbAltura;
+        private System.Windows.Forms.Button btnClacular;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblSaude;
     }
 }
 
--- CalculadoraIMC/Form1.cs
+++ CalculadoraIMC/Form1.cs
@@ -16,5 +16,89 @@ namespace CalculadoraIMC
         {
             InitializeComponent();
         }
+
+        private void btnClacular_Click(object sender, EventArgs e)
+        {
+            //VERIFICAR SE OS CAMPOS FORAM PREENCHIDOS CORRETAMENTE:
+
+            if (txbPeso.Text == "")
+            {
+                MessageBox.Show("Preencha o campo 'Peso'", "Erro!",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //Mudar a cor do fundo e a cor da letra,
+                //indicando o campo a ser preenchido:
+                txbPeso.BackColor = Color.Red;
+                txbPeso.ForeColor = Color.White;
+
+
+            }
+            else if (txbAltura.Text == "")
+            {
+                MessageBox.Show("Preencha o campo 'Altura'", "Erro!",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //Mudar a cor do fundo e a cor da letra,
+                //indicando o campo a ser preenchido:
+                txbAltura.BackColor = Color.Red;
+                txbAltura.ForeColor = Color.White;
+
+
+            }
+            else if (txbAltura.Text.Length < 3)
+            {
+                MessageBox.Show("O campo 'Altura' precisa ter no mínimo três caracteres",
+                "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+
+            }
+            else //SE A VERIFICAÇÃO FOR BEM SUCEDIDA, INICIA O PROGRAMA:
+            {
+                //Declaração de variaveis que serão utilizadas:
+                double altura = double.Parse(txbAltura.Text);
+                double peso = double.Parse(txbPeso.Text);
+                double result;
+
+                //Calculo do IMC peso/(altura * altura):
+                result = peso/(altura * altura);
+
+                //Verificar a classificação do usuário
+                if (result < 18.5)
+                {
+                    lblResultado.Text = ($"IMC: {Math.Round(result, 2)}");
+                    lblSaude.Text = ("Abaixo do peso");
+                }
+                else if (result > 18.5 && result < 25.0)
+                {
+                    lblResultado.Text = ($"IMC: {Math.Round(result, 2)}");
+                    lblSaude.Text = ("Peso ideal (Parabéns!)");
+                }
+                else if (result > 24.9 && result < 30.0)
+                {
+                    lblResultado.Text = ($"IMC: {Math.Round(result, 2)}");
+                    lblSaude.Text = ("levemente acima do peso");
+                }
+                else if (result > 29.9 && result < 35.0)
+                {
+                    lblResultado.Text = ($"IMC: {Math.Round(result, 2)}");
+                    lblSaude.Text = ("Obesidade Grau I");
+                }
+                else if (result > 34.9 && result < 40.0)
+                {
+                    lblResultado.Text = ($"IMC: {Math.Round(result, 2)}");
+                    lblSaude.Text = ("Obesidade Grau II (Severa)");
+                }
+                else
+                {
+                    lblResultado.Text = ($"IMC: {Math.Round(result, 2)  }");
+                    lblSaude.Text = ("Obesidade Grau III (Mórbida)");
+                }
+
+                //Retornar o campo txbProdutos à cor normal quando
+                //o campo for preenchido corretamente:
+                txbPeso.BackColor = Color.White;
+                txbPeso.ForeColor = Color.Black;
+                txbAltura.BackColor = Color.White;
+                txbAltura.ForeColor = Color.Black;
+            }
+        }
     }
 }
